7ad38e4713cc23946b75345a4128881c867e5b77
Salauni-Patel <salaunipatel2295@gmail.com>
2017-11-30 20:46:57 -0800

    admin side APIs added

--- server/app.js
+++ server/app.js
@@ -7,8 +7,11 @@ var mysql = require('mysql');
 var ObjectId = require('mongodb').ObjectID;
 var urlencodedPraser=bodyParser.urlencoded({extended:false});
 var status;
-var auth = require('passport-local-authenticate');
-var rn = require('random-number');
+//var auth = require('passport-local-authenticate');
+//var rn = require('random-number');
+
+var redisClient = require('redis').createClient;
+var redis = redisClient(6379, 'localhost');
 
 app.use(express.bodyParser());
 
@@ -43,14 +46,19 @@ MongoClient.connect('mongodb://kayak:kayak273@ds259175.mlab.com:59175/kayak', (e
       res.sendFile(__dirname + '/index.html')
   })
 
-  app.get('/list', (req, res) => {
-   // var cursor = db.collection('quotes').find()
+  //app.post('/booking',mysqldbservice.putdata);
+
+
+  app.get('/listofflights', (req, res) => {
     db.collection('flightsData').find().toArray(function(err, results) {
         console.log(results)
-        // send HTML file populated with quotes here
+       
       })
   })
 
+
+
+/*
  app.get('/search', (req, res) => {
     // var cursor = db.collection('quotes').find()
      db.collection('flightsData').find({"destination":"Boston", "Date": "2017-11-01", "source": "Phoenix"}).toArray(function(err, results) {
@@ -58,28 +66,235 @@ MongoClient.connect('mongodb://kayak:kayak273@ds259175.mlab.com:59175/kayak', (e
          // send HTML file populated with quotes here
        })
    })
+*/
 
- app.get('/search', (req, res) => {
-    // var cursor = db.collection('quotes').find()
-     db.collection('flightsData').find({"destination":"Boston", "Date": "2017-11-01", "source": "Phoenix"}).toArray(function(err, results) {
+findBookByTitleCached = function (db, redis, Boston, callback) {
+    redis.get("Philadelphia", function (err, reply) {
+        if (err) callback(null);
+        else if (reply) //Book exists in cache
+        callback(JSON.parse(reply));
+        else {
+            //Book doesn't exist in cache - we need to query the main database
+            db.collection('flightsData').findOne({
+                source: "Philadelphia"
+            }, function (err, doc) {
+                if (err || !doc) callback(null);
+                else {
+                    redis.set("Philadelphia", JSON.stringify(doc), function () {
+                        callback(doc);
+                    });
+                }
+            });
+        }
+    });
+};
+
+
+app.get('/search1', function (req, res) {
+   
+    
+        findBookByTitleCached(db, redis, "Philadelphia", function (book) {
+            
+            res.status(200).send(book);
+        });
+    
+});
+
+
+
+
+findflightsCached = function (db, redis, source,destination, callback) {
+    redis.get(source,destination, function (err, reply) {
+        if (err) callback(null);
+        else if (reply) //Book exists in cache
+        callback(JSON.parse(reply));
+        else {
+            //Book doesn't exist in cache - we need to query the main database
+            db.collection('flightsData').findOne({
+                source: source,
+                destination : destination
+            }, function (err, doc) {
+                if (err || !doc) callback(null);
+                else {
+                    redis.set(destination, JSON.stringify(doc), function () {
+                        console.log(doc);
+                        callback(doc);
+
+                    });
+
+                    redis.set(source, JSON.stringify(doc), function () {
+                        console.log(doc);
+                        callback(doc);
+
+                    });
+                }
+            });
+        }
+    });
+};
+
+
+
+
+app.post('/search2', function (req, res) {
+    
+     var source = req.body.source;
+     var destination = req.body.destination;
+     console.log(destination);
+     findflightsCached(db, redis, source,destination, function (book) {
+             
+             res.status(200).send(book);
+         });
+     
+ });
+
+
+
+
+
+
+
+
+ app.post('/searchflights', (req, res) => {
+    
+   var destination= req.body.destination;
+    var date= req.body.date;
+    var source= req.body.source;
+    
+    console.log(req.body)
+    console.log(destination)
+    console.log(req.body.destination)
+ /*
+    var flightdata = {
+        "destination":req.body.destination,
+        "Date":req.body.date, 
+        "source":req.body.source
+    };
+
+ console.log(flightdata)
+*/
+   // db.collection('flightsData').find(flightdata).toArray(function(err,results){
+
+    db.collection('flightsData').find({"destination" : destination, "source" : source, "Date": date }).toArray(function(err, results) {
+         console.log(results);
+          
+       })
+   });
+
+
+   app.post('/searchhotels', (req, res) => {
+   
+    var roomtype= req.body.roomtype;
+   var hoteladdress= req.body.hoteladdress;
+    console.log(roomtype)
+    console.log(req.body)
+   
+    db.collection('hotels').find({"hoteladdress" : hoteladdress, "roomtype" : roomtype}).toArray(function(err, results) {
+        console.log(results);
+         
+      })
+  });
+
+ 
+    
+
+   app.post('/user_cc_details',function(req,res){
+    
+        var cardnumber = req.body.cardnumber;
+       
+        console.log(req.body);
+    
+    });
+
+   app.post('/flightdetails', (req, res) => {
+    
+    var destination= req.body.destination;
+    var date= req.body.date;
+    var source= req.body.source;
+    var flightname=req.body.flightname;
+
+     db.collection('flightsData').findOne({"destination":destination, "Date": date, "source": source, "fligtName": flightname},function(err, results) {
          console.log(results)
-         // send HTML file populated with quotes here
+
        })
    })
 
-   app.get('/details', (req, res) => {
-    // var cursor = db.collection('quotes').find()
-     db.collection('flightsData').findOne({"destination":"Boston", "Date": "2017-11-01", "source": "Phoenix", "fligtName": "Virgin America"},function(err, results) {
+
+   app.post('/hoteldetails', (req, res) => {
+    
+    var roomtype= req.body.roomtype;
+    var hoteladdress= req.body.hoteladdress;
+    var hotelname=req.body.hotelname;
+
+     console.log(roomtype)
+     console.log(req.body)
+
+     db.collection('hotels').findOne({"hoteladdress" : hoteladdress, "roomtype" : roomtype, "hotelname": hotelname},function(err, results) {
          console.log(results)
-         console.log(results._id)
+
        })
    })
 
 
-   //app.get('/userinfo',mysqldbservice.getUserInfo)
 
-   //flight booking page APIs
+   
+   app.post('/searchcar', (req, res) => {
+    
+    var cartype= req.body.cartype;
+    var pickupcity= req.body.pickupcity;
+    
+     console.log(cartype)
+     console.log(req.body)
 
+     db.collection('cars').findOne({"pickupcity" : pickupcity, "cartype" : cartype},function(err, results) {
+         console.log(results)
+
+       })
+   })
+
+
+   
+   app.post('/cardetails', (req, res) => {
+    
+    var cartype= req.body.cartype;
+    var pickupcity= req.body.pickupcity;
+    var carbrand=req.body.carbrand;
+     console.log(cartype)
+     console.log(req.body)
+
+     db.collection('cars').findOne({"pickupcity" : pickupcity, "cartype" : cartype, "carbrand": carbrand},function(err, results) {
+         console.log(results)
+
+       })
+   })
+
+   app.get('/detailstest', details, function(req,res){
+    db.collection('flightsData').findOne({"destination":"Boston", "Date": "2017-11-01", "source": "Phoenix", "fligtName": "Virgin America"},function(err, results) {
+        console.log( results)
+        res.send (results.toString())
+       /*
+        var rows=results;
+        var jsonString= JSON.stringfy(results);
+        res.send (jsonString) 
+        */ 
+    })
+   })
+
+
+function details(req, res, next){
+   
+    console.log("results are:" + results)
+
+}
+
+  app.get('/details1', (req, res) => {
+      console.log("results are:" + results);
+  })
+
+
+   app.get('/userinfo',mysqldbservice.getUserInfo)
+
+ 
 //Amita's code
 
 //Admin modele
@@ -114,6 +329,67 @@ app.post('/addFlightdata',urlencodedPraser,function(req,res){
     });
     res.json({staus:status});
 });
+//salauni 
+//admin API for adding hotels
+app.post('/addHoteldata',urlencodedPraser,function(req,res){
+    
+        var coll = db.collection('hotels');
+    
+        console.log(req);
+        console.log(req.body.duration);
+    
+        var hoteldata={ 
+        "hotelid": req.body.hotelid,
+        "hotelname": req.body.hotelname,
+        "roomtype": req.body.roomtype,
+        "hoteladdress": req.body.hoteladdress,
+        "availablerooms": req.body.availablerooms,
+        "noofguestsperroom": req.body.noofguestsperroom,
+        "hotelbaseprice": req.body.hotelbaseprice,
+        "hoteltotalprice": req.body.hoteltotalprice
+        };
+        coll.insert(hoteldata,function(err,result){
+            if(err){
+                console.log('error: '+err);
+            }
+            console.log('record inserted');
+            
+            status=2;
+            console.log("Data inserted successfully");
+        });
+        res.json({staus:status});
+    });
+    
+//salauni 
+//admin API for adding cars
+app.post('/addCardata',urlencodedPraser,function(req,res){
+    
+        var coll = db.collection('cars');
+    
+        console.log(req);
+       
+    
+        var cardata= { 
+            "carid": req.body.carid,
+            "cartype": req.body.cartype,
+            "carbrand": req.body.carbrand,
+            "carbaseprice": req.body.carbaseprice,
+            "cartotalprice": req.body.cartotalprice,
+            "pickupcity": req.body.pickupcity,
+            "pickupaddress": req.body.pickupaddress
+         };
+        coll.insert(cardata,function(err,result){
+            if(err){
+                console.log('error: '+err);
+            }
+            console.log('record inserted');
+            
+            status=2;
+            console.log("Data inserted successfully");
+        });
+        res.json({staus:status});
+    });
+
 
 //update flight data
 app.post('/updateFlightdata',urlencodedPraser,function(req,res){
@@ -146,6 +422,67 @@ app.post('/updateFlightdata',urlencodedPraser,function(req,res){
     });
     res.json({staus:status});
 });
+
+//salauni
+// Admin API for updating hotels
+app.post('/updateHoteldata',urlencodedPraser,function(req,res){
+    
+        var coll = db.collection('hotels');
+        var hotelid={_id:ObjectId(req.body.hotelid)};
+        var hoteldata={$set:
+            {
+                "hotelname": req.body.hotelname,
+                "roomtype": req.body.roomtype,
+                "hoteladdress": req.body.hoteladdress,
+                "availablerooms": req.body.availablerooms,
+                "noofguestsperroom": req.body.noofguestsperroom,
+                "hotelbaseprice": req.body.hotelbaseprice,
+                "hoteltotalprice": req.body.hoteltotalprice
+               
+            }
+        };
+        coll.update(hotelid,hoteldata,function(err,result){
+            if(err){
+                console.log('error: '+err);
+            }
+            console.log('record updated');
+           
+            status=2;
+            console.log("Data updated successfully");
+        });
+        res.json({staus:status});
+    });
+    
+// Admin API for updating cars
+app.post('/updateCardata',urlencodedPraser,function(req,res){
+    
+        var coll = db.collection('cars');
+        var carid={_id:ObjectId(req.body.carid)};
+        var cardata={$set:
+            {
+                "cartype": req.body.cartype,
+                "carbrand": req.body.carbrand,
+                "carbaseprice": req.body.carbaseprice,
+                "cartotalprice": req.body.cartotalprice,
+                "pickupcity": req.body.pickupcity,
+                "pickupaddress": req.body.pickupaddress
+            }
+        };
+        coll.update(carid,cardata,function(err,result){
+            if(err){
+                console.log('error: '+err);
+            }
+            else
+            {
+            console.log('record updated');
+           
+            status=2;
+            console.log("Data updated successfully");
+        }
+        });
+        res.json({staus:status});
+    });
+
 
 //update user account by admin
 
@@ -242,6 +579,9 @@ app.post('/addBookingDetails',urlencodedPraser,function(req,res){
             req.body.passenger_details[i].flightpassengerfirstname + "','" + req.body.passenger_details[i].flightpassengerlastname + "','" + req.body.passenger_details[i].flightpassengergender+ "','" + req.body.journeydate + "','" +
             req.body.flight.flightid + "','" + req.body.flight.airlines + "','" + req.body.flightclass + "')";
 
+
+
+
         console.log("Query is:" + saveUser);
         status = 0;
 
@@ -269,6 +609,107 @@ app.post('/addBookingDetails',urlencodedPraser,function(req,res){
 
     res.json({staus:status});
 });
+
+
+//salauni
+//hotel booking API
+
+app.post('/hotelBookingDetails',urlencodedPraser,function(req,res){
+    
+        var options = {
+            min:  0,
+            max:  10000,
+            integer: true
+        }
+        var trip_id = rn(options);
+        console.log(trip_id);
+        var saveUser;
+     
+            
+    saveUser = " INSERT INTO hotelbookingdetails (`email`, `tripid`, `hotelname`, `roomtype`, `hoteladdress`, `checkindate`, `checkoutdate`, `noofguests`, `hotelbaseprice`, `hoteltotalprice`) VALUES ( '" +
+req.body.email + "','" + trip_id + "','" + req.body.hotel.hotelname + "','" + req.body.hotel.roomtype + "','" + req.body.hotel.hoteladdress + "','" + req.body.checkindate + "','" + req.body.checkoutdate + "','" +
+req.body.noofguests + "','" + req.body.hotelbaseprice + "','" + req.body.hoteltotalprice + "' ) ";
+
+
+
+            console.log("Query is:" + saveUser);
+            status = 0;
+    
+            mysqldbservice.insertData(function (err, results) {
+                if (err) {
+                    throw err;
+                }
+                else {
+                    status = 2;
+                    console.log("Data inserted successfully");
+                }
+            }, saveUser);
+        
+    
+        var saveTripdata="insert into tripdetails values('"+trip_id+"','"+req.body.email+"','"+req.body.bookingtype+"',"+req.body.hoteltotalamount+",'"+new Date().getDate().toString()+"')"
+        mysqldbservice.insertData(function (err, results) {
+            if (err) {
+                throw err;
+            }
+            else {
+                status = 2;
+                console.log("Data inserted successfully");
+            }
+        }, saveTripdata);
+    
+        res.json({staus:status});
+    });
+    
+    
+//car booking API
+
+app.post('/carBookingDetails',urlencodedPraser,function(req,res){
+    
+        var options = {
+            min:  0,
+            max:  10000,
+            integer: true
+        }
+        var trip_id = rn(options);
+        console.log(trip_id);
+        var saveUser;
+     
+            
+
+saveUser = " INSERT INTO carbookingdetails (`email`, `tripid`, `carid`, `pickupdate`, `dropoffdate`, `carbaseprice`, `cartotalprice`, `caruserfirstname`, `caruserlastname`, `pickupcity`, `dropoffcity`) VALUES ( '" +
+req.body.email + "','" + trip_id + "','" + req.body.car.carid + "','" + req.body.pickupdate + "','" + req.body.dropoffdate + "','"+ req.body.carbaseprice + "','" + req.body.cartotalprice + "','"+ req.body.caruserfirstname + "','" + req.body.caruserlastname + "','"+ req.body.pickupcity + "','" + req.body.dropoffcity + "'  );";
+
+            console.log("Query is:" + saveUser);
+            status = 0;
+    
+            mysqldbservice.insertData(function (err, results) {
+                if (err) {
+                    throw err;
+                }
+                else {
+                    status = 2;
+                    console.log("Data inserted successfully");
+                }
+            }, saveUser);
+      
+    
+        var saveTripdata="insert into tripdetails values('"+trip_id+"','"+req.body.email+"','"+req.body.bookingtype+"',"+req.body.cartotalamount+",'"+new Date().getDate().toString()+"')"
+        mysqldbservice.insertData(function (err, results) {
+            if (err) {
+                throw err;
+            }
+            else {
+                status = 2;
+                console.log("Data inserted successfully");
+            }
+        }, saveTripdata);
+    
+        res.json({staus:status});
+    });
+    
+
+
+
 
 //get ticket details
 app.post('/getFlightTicket',urlencodedPraser,function(req,res){
@@ -312,6 +753,28 @@ app.post('/getHotelTicket',urlencodedPraser,function(req,res){
             console.log("data:"+results)
             status=2;
             //console.log("Data inserted successfully");
+        }
+    },selectUser);
+    console.log("data:"+data)
+    res.json({staus:status});
+});
+
+//get car booking receipt
+app.post('/getCarreceipt',urlencodedPraser,function(req,res){
+    var selectUser="select * from tripdetails td " +
+        "inner join carbookingdetails car on car.tripid=td.tripid where td.tripid='"+req.body.tripid+"'";
+    console.log("Query is:"+selectUser );
+    var status=0;
+    var result;
+
+    var data=mysqldbservice.getUserInfo(function(err,results){
+        if(err){
+            throw err;
+        }
+        else
+        {
+            console.log("data:"+results)
+            status=2;
         }
     },selectUser);
     console.log("data:"+data)
