9b3417c78f13a9b14cdd0f9c9755b0b4caf4de1b
Nidhi Narendra <nidhi.narendra1@gmail.com>
2017-12-03 00:11:07 -0800

    hotel rooms and guests fixed

--- server/public/angularjs/hotels.js
+++ server/public/angularjs/hotels.js
@@ -2,41 +2,27 @@ var hotels = angular.module('hotels', []);
 // defining the login controller
 hotels.controller('hotels', function($scope, $http) {
   console.log('in hotels angular');
-  $scope.loadHotels = function() {
-    console.log('in angular load hotels');
-    $http({
-      method: 'POST',
-      url: '/bookHotel',
-      data: $scope.hotelList[index]
-    })
-      .success(function(data) {
-        console.log(data);
-        window.location.assign('/hotelPayment');
-      })
-      .error(function(error) {
-        alert('error');
-      });
-    //api or db
-    $scope.hotelList = [
-      {
-        title: 'abc',
-        location: 'sjc',
-        price: '100',
-        ratingNumber: '4',
-        reviews: '10000'
-      },
-      {
-        title: 'abc1',
-        location: 'sjc1',
-        price: '1001',
-        ratingNumber: '4.1',
-        reviews: '100001'
-      }
-    ];
 
-    //on success of api
-    $scope.numberOfHotels = $scope.hotelList.length;
-  };
+  //api or db
+  $scope.hotelList = [
+    {
+      title: 'abc',
+      location: 'sjc',
+      price: '100',
+      ratingNumber: '4',
+      reviews: '10000'
+    },
+    {
+      title: 'abc1',
+      location: 'sjc1',
+      price: '1001',
+      ratingNumber: '4.1',
+      reviews: '100001'
+    }
+  ];
+
+  //on success of api
+  $scope.numberOfHotels = $scope.hotelList.length;
 
   $scope.bookHotel = function(index) {
     console.log($scope.hotelList[index]);
